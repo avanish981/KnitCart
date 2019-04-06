211dd10ca3702eaaab4e7b88a68b0cec36d11ee5
AVANISH PRATAP SINGH <41297065+avanish981@users.noreply.github.com>
2019-04-06 08:03:41 +0530

    Update models.py

--- sbook/shop/models.py
+++ sbook/shop/models.py
@@ -4,11 +4,9 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50, default="")
-    subcategory = models.CharField(max_length=50, default="")
+    category = models.CharField(max_length=50,default="")
     price = models.IntegerField(default=0)
-    desc = models.CharField(max_length=300)
-    pub_date = models.DateField()
+    desc = models.CharField(max_length=300,default="")
     image = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
@@ -31,8 +29,4 @@ class Orders(models.Model):
     items_json = models.CharField(max_length=5000)
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
-    address = models.CharField(max_length=111)
-    city = models.CharField(max_length=111)
-    state = models.CharField(max_length=111)
-    zip_code = models.CharField(max_length=111)
     phone = models.CharField(max_length=111, default="")
