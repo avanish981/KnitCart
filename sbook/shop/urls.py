c1bd23768c7698abdacc5509202090c111450452
AVANISH PRATAP SINGH <41297065+avanish981@users.noreply.github.com>
2019-04-07 19:29:37 +0530

    Update urls.py

--- sbook/shop/urls.py
+++ sbook/shop/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("search/", views.search, name="Search"),
     path("products/<int:myid>", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
+    path("product/",views.product,name="product")
 ]
