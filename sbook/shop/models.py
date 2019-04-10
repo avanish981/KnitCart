b54b8a2368dc25234f6bde53615c9f69e93df4ae
AVANISH PRATAP SINGH <41297065+avanish981@users.noreply.github.com>
2019-04-10 22:50:03 +0530

    Update models.py

--- sbook/shop/models.py
+++ sbook/shop/models.py
@@ -9,6 +9,9 @@ class Product(models.Model):
     desc = models.CharField(max_length=300,default="")
     image = models.ImageField(upload_to='shop/images', default="")
 
+    class Meta:
+         db_table="Product"
+
     def __str__(self):
         return self.product_name
 
@@ -19,6 +22,8 @@ class Contact(models.Model):
     email = models.CharField(max_length=70, default="")
     phone = models.CharField(max_length=70, default="")
     desc = models.CharField(max_length=500, default="")
+    class Meta:
+        db_table="Contact"
 
 
     def __str__(self):
@@ -29,4 +34,20 @@ class Orders(models.Model):
     items_json = models.CharField(max_length=5000)
     name = models.CharField(max_length=90)
     email = models.CharField(max_length=111)
+    room_no=models.CharField(max_length=111,default="")
+    year=models.CharField(max_length=111,default="")
     phone = models.CharField(max_length=111, default="")
+    class Meta:
+        db_table="Order"
+class Signup(models.Model):
+    username=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    class Meta:
+        db_table="Signup"
+    def __str__(self):
+        return self.username
+class First(models.Model):
+    usernmae=models.CharField(max_length=100)
+    class First:
+        db_table="First"
